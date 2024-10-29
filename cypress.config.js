fd932b03234490d6512ff2465ab80dee55759dd1
sreeMadamx <sreekanthm1947@gmail.com>
2024-10-29 17:14:36 +0530

    updated files

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
@@ -5,18 +7,37 @@ const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esb
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 async function setupNodeEvents(on, config) {
-  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  // Add Allure plugin
+  allureWriter(on, config);
+
+  // Add Cucumber preprocessor plugin for JSON reports
   await preprocessor.addCucumberPreprocessorPlugin(on, config);
 
+  // Set up file bundler for Cucumber preprocessor
   on(
     "file:preprocessor",
     createBundler({
       plugins: [createEsbuildPlugin.default(config)],
     })
   );
-  allureWriter(on, config);
 
-  // Make sure to return the config object as it might have been modified by the plugin.
+  // Delete screenshots folder before running tests
+  on('before:run', () => {
+    const screenshotDir = path.join(config.projectRoot, 'cypress', 'screenshots');
+    console.log(`Attempting to delete screenshot folder: ${screenshotDir}`);
+
+    if (fs.existsSync(screenshotDir)) {
+      try {
+        fs.rmdirSync(screenshotDir, { recursive: true });
+        console.log("Screenshots folder deleted successfully.");
+      } catch (error) {
+        console.error("Error deleting screenshots folder:", error);
+      }
+    } else {
+      console.log("Screenshots folder does not exist.");
+    }
+  });
+
   return config;
 }
 
@@ -24,8 +45,10 @@ module.exports = defineConfig({
   projectId: 'bn1uf4',
   e2e: {
     setupNodeEvents,
+    baseUrl: "https://opensource-demo.orangehrmlive.com",
     specPattern: "cypress/e2e/features/*.feature",
-    baseUrl: "https://www.saucedemo.com",
+    screenshotsFolder: 'cypress/screenshots',
+    screenshotOnRunFailure: true,
     chromeWebSecurity: false,
     env: {
       allureReuseAfterSpec: true,
--- cypress/e2e/features/tags.feature
+++ cypress/e2e/features/tags.feature
@@ -4,10 +4,12 @@ Feature: Login page with Tags
 
     Background:
         Given A web browser is at the saucelabs login page - tag demo
+
     @mobile
     Scenario: Success Login
         When A user enters the username "standard_user", the password "secret_sauce", and clicks on the login button - tag demo
         Then the url will contains the inventory subdirectory - tag demo
+        
     @desktop
     Scenario: Blocked Login
         When A user enters the username "locked_out_user", the password "secret_sauce", and clicks on the login button - tag demo
